f3059b6eebc97d8c37fbe4b5a8eb3e4d46ef19e6
Abdulxoliq <abdulxoliq@Abdulxoliqs-MacBook-Pro.local>
2022-12-17 21:09:19 +0500

    create profile page finished

--- src/screens/add-friends/add-friends.js
+++ src/screens/add-friends/add-friends.js
@@ -15,21 +15,25 @@ const data = [
     image:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
     name: 'Melissa Johnson',
+    id: 0,
   },
   {
     image:
       'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
     name: 'David Torres',
+    id: 1,
   },
   {
     image:
       'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
     name: 'Marilyn Lewis',
+    id: 2,
   },
   {
     image:
       'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
     name: 'Tessa Hard',
+    id: 3,
   },
 ];
 
@@ -63,8 +67,46 @@ const AddFriends = () => {
         </Text>
         <ScrollView style={{marginTop: 30, height: '100%'}}>
           <View>
-            {data.map(({name, image}) => (
+            {data.map(({name, image, id}) => (
               <View
+                key={id}
+                style={[
+                  styles.userStyle,
+                  {
+                    justifyContent: 'space-between',
+                    borderBottomColor: '#ddd',
+                    borderBottomWidth: 1,
+                    paddingVertical: 20,
+                  },
+                ]}>
+                <View style={styles.userStyle}>
+                  <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <Image
+                      style={{height: 50, width: 50, borderRadius: 100}}
+                      source={{uri: image}}
+                    />
+                    <Text style={{fontSize: 18, marginLeft: 10}}>{name}</Text>
+                  </View>
+                </View>
+                <TouchableOpacity onPress={() => setFriend(!friend)}>
+                  <Text
+                    style={{
+                      color: friend ? '#E57814' : '#C5C5CD',
+                      fontSize: 18,
+                    }}>
+                    {friend ? 'Add friend' : 'Added'}
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </View>
+          <View style={{marginTop: 20}}>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>
+              Invite people
+            </Text>
+            {data.map(({name, image, id}) => (
+              <View
+                key={id}
                 style={[
                   styles.userStyle,
                   {
